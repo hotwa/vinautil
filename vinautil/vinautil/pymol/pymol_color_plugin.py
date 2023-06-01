30bf20dcab6b36b78843ec6b9b96283dbd40a2e5
lyzeng <pylyzeng@gmail.com>
2023-06-01 10:01:44 +0800

    update

--- vinautil/vinautil/pymol/pymol_color_plugin.py
+++ vinautil/vinautil/pymol/pymol_color_plugin.py
@@ -3,7 +3,7 @@
 '''
 @file        :pymol_color_plugin.py
 @Description:       :pymol配色方案
-@Date     :2021/11/10 10:52:25
+@Date     :2023/6/1 10:52:25
 @Author      :hotwa
 @version      :1.0
 '''
@@ -12,7 +12,7 @@
 from pymol import cmd
 from pathlib import Path
 
-class color_plan():
+class colorp():
     color1 = (('color1', '[186,182,217]'), 'purple')
     color2 = (('color2', '[233,195,153]'), 'yellow')
     color3 = (('color3', '[43,113,216]'), 'blue-N')
@@ -38,19 +38,20 @@ class color_plan():
 
     def pretty(self):
         cmd.remove('solvent metals') # 移除金属离子和溶剂
+        cmd.remove("resn SO4 or resn PO4 or resn CL")
         cmd.pretty(selection=self.id)
 
     @staticmethod
     def defcolor():
         # 定义常用配色
-        color_gennerate = map(lambda x:x[0],color_plan.colors)
+        color_gennerate = map(lambda x:x[0],colorp.colors)
         list(map(lambda x:cmd.set_color(*x),color_gennerate))
         # 定义灰度配色
-        cmd.set_color(*color_plan.grey1)
+        cmd.set_color(*colorp.grey1)
 
     @staticmethod
     def grey(obj='sele',opt=True):
-        color_plan.defcolor()
+        colorp.defcolor()
         if opt: method.optimisation() # 优化
         # 对某个对象进行灰化
         cmd.color('grey1',f'{obj}')
@@ -58,7 +59,7 @@ class color_plan():
     @staticmethod
     def color_mole(obj='hetatm'):
         # color blue,sele in name c*
-        color_plan.defcolor()
+        colorp.defcolor()
         cmd.color('color9',f'{obj} in name c*')
         cmd.color('color6',f'{obj} in name o*')
         cmd.color('color5',f'{obj} in name s*')
@@ -86,8 +87,8 @@ class method():
 
     @staticmethod
     def optimisation(grey=True):
-        color_plan.defcolor()
-        print('常规优化')# 常规优化
+        colorp.defcolor()
+        print('color optimisation')# 常规优化
         if grey: cmd.set('cartoon_color','grey1')
         cmd.set('stick_transparency','0.1')
         cmd.set("ray_shadows","off") # 消除投射阴影
@@ -97,8 +98,44 @@ class method():
 
     @staticmethod
     def remove():
+        cmd.remove('resn SO4') # SO4 remove
         cmd.remove('solvent metals') # 移除金属离子和溶剂
-        
+      
+def get_resn(name:str = 'all'):
+    resn_list = []
+    cmd.iterate(selection=name, expression=lambda atom: resn_list.append(atom.resn))
+    return tuple(set(resn_list))      
 
-cmd.extend('color_plan',color_plan)
+
+def protgrey():
+    method.remove()
+    method.optimisation()
+    
+# 观察小分子
+# protgrey()
+# colorp.color_mole('sele')
+# add label
+# label sele, "your label"
+# black background
+# set bg_rgb, [0, 0, 0]
+# define label
+# cmd.get_position("sele")
+# set label_position, [x,y,z]
+# cmd.h_add(pymol_obj) # add all hydrogens in this molecular
+# cmd.remove(f"{pymol_obj} & hydro & not nbr. (don.|acc.)") # remove no-polar-hydrogen
+# cmd.remove(f"sele & hydro") # 移除所有的H
+# cmd.set('cartoon_color','color8', 'obj')
+# cmd.hide("everything", "chain A and resn UHT and alt B") # 小分子隐藏B构象
+# cmd.label("chain A and resn UHT and alt A", "resn") # 添加标签
+#cmd.select('sele', f'resn {resn} and chain {chain}')
+#cmd.center('sele')
+#cmd.orient('sele')
+#obj_name = 'around_and_self'
+# cmd.create(obj_name, f'(resn {resn} and chain {chain}) around {distance}') # 不扩展残基
+# cmd.create(obj_name, f'byres (resn {resn} and chain {chain}) around {distance}') # 扩展残基
+# cmd.create(obj_name, f'(resn {resn} and chain {chain}) around {distance} or (resn {resn} and chain {chain})') # 选择resn的对象和resn对象周围3A的原子
+#cmd.create(obj_name, f'byres (resn {resn} and chain {chain}) around {distance} or (resn {resn} and chain {chain})') # 选择resn的对象和resn对象周围3A的原子扩展至残基的原子
+#cmd.hide('everything', 'all')
+#cmd.show('lines', obj_name)
+cmd.extend('colorp',colorp)
 cmd.extend('method',method)
