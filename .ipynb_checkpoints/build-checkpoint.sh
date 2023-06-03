c726b7850c907efe20e87c2b58f876c4c1845afa
lyzeng <pylyzeng@gmail.com>
2023-06-03 10:37:02 +0800

    remove

--- .ipynb_checkpoints/build-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-# install shell script for the project
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
-git clone https://github.com/forlilab/Meeko
-cd Meeko
-$PYTHON -m pip install .  --no-deps --ignore-installed -vv
