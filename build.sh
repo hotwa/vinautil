475aefa77475906c775218a6bda70a973facf2f8
hotwa <pylyzeng@gmail.com>
2023-12-27 16:51:35 +0800

    change mirror

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # install shell script for the project
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
-git clone https://github.com/forlilab/Meeko
+git clone https://mirror.ghproxy.com/https://github.com/forlilab/Meeko
 cd Meeko
 $PYTHON -m pip install .  --no-deps --ignore-installed -vv
